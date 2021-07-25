beb2d26b5ad5dc9a7ce278259076bbb7305dbd4c
Matt Pletcher <matt.pletch@gmail.com>
2021-07-25 16:34:51 -0400

    Add initial spring web project files

---
+++ src/main/java/com/mp/fantasymagicstocks/FantasyMagicStocksApplication.java
@@ -0,0 +1,13 @@
+package com.mp.fantasymagicstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyMagicStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyMagicStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mp/fantasymagicstocks/FantasyMagicStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.fantasymagicstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyMagicStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
